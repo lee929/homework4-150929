3519b76a6b499870b6efc629c1f05845e94cd698
ivorylee29 <ko0kiemonster29@172-16-29-237.DYNAPOOL.NYU.EDU>
2015-09-30 02:28:55 -0400

    first commit

---
+++ annoyingweb/js/index.js
@@ -0,0 +1,6 @@
+var popup = document.querySelector('#popup');
+
+var defaultButton = document.querySelector('#defaultButton')
+
+defaultButton.onclick = function() {
+    popup.classList.add('show')}
